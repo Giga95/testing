b94e1726ad5f20e282c9b4cd227797e70cfdf54f
unknown <djurasinovicdragisa@gmail.com>
2023-03-01 12:19:44 +0100

    fix

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./js");
   eleventyConfig.addPassthroughCopy("./php");
   eleventyConfig.addPassthroughCopy("./img");
+  eleventyConfig.addPassthroughCopy("./admin");
+
   return {
     dir: {
       input: "./",
